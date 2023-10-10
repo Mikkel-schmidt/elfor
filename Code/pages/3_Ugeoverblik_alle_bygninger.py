5fc96d20ce13c0ed23905fcb687dabb55ef2b141
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2023-10-10 11:05:09 +0200

    IDs

--- Code/pages/3_Ugeoverblik_alle_bygninger.py
+++ Code/pages/3_Ugeoverblik_alle_bygninger.py
@@ -56,10 +56,10 @@ df['ugedag'] = df['from'].dt.day_name(locale='da_DK')
 #st.write(df['meter'].unique().isin(list(st.session_state.valgt_meter)))
 
 
-col1, col2 = st.columns([1,4])
 
-col1.header('Ugeoverblik')
 
+st.header('Ugeoverblik')
+st.write(IDs)
 
 #@st.cache_resource
 def heatmapp(df):
@@ -106,57 +106,11 @@ def heatmapp(df):
     )
     return b1
 
-col1, col2 = st.columns([4,1])
-col2.subheader('Heatmap og ugeprofil')
-col2.markdown("""I figuren til venstre kan man se hvordan forbruget fordeler sig henover timerne på en uge.
-Her burde man kunne se hvordan åbningstiderne i bygningen er og om bygningen lukkes ned udenfor åbningstid""")
-col2.markdown('Tallet i midten er det gennemsnitlige forbrug i den time på den ugedag.')
-
-if df['bkps'].iloc[-1] >= df['bkps'].max():
-    df_opti = df[df['bkps']==df['bkps'].iloc[-1]].groupby('from').agg({'meter': 'mean', 'amount': 'sum', 'day-moment': 'first'}).reset_index()
-else:
-    df_opti = df[df['bkps']==df['bkps'].min()].groupby('from').agg({'meter': 'mean', 'amount': 'sum', 'day-moment': 'first'}).reset_index()
-
-df_norm = df[df['bkps']==df['bkps'].iloc[-1]].groupby('from').agg({'meter': 'mean', 'amount': 'sum', 'day-moment': 'first'}).reset_index()
-
-with col1:
-    figure = heatmapp(df.iloc[-2159:])
-    st_pyecharts(figure, height='400px', key='hej')
-
-#@st.cache_data
-def ugeprofil(df):
-    dff = df.groupby([df['from'].dt.day_name(locale='da_DK'), df['from'].dt.hour]).agg({'amount': ['mean', 'std']}).reset_index(names=['day', 'hour'])
-    dff.columns = ['_'.join(tup).rstrip('_') for tup in dff.columns.values]
-    #st.write(dff)
-    dff['day_'] = dff['day']
-    dff['day_'].replace({
-            "Mandag": 0,
-            "Tirsdag": 1,
-            "Onsdag": 2,
-            "Torsdag": 3,
-            "Fredag": 4,
-            "Lørdag": 5,
-            "Søndag": 6},
-            inplace=True,)
-    dff.sort_values(['day_', 'hour'], ascending=True, inplace=True)
-    #st.write(dff)
-    dff['x-axis'] = dff.apply(lambda row: row['day'] + ' kl. ' + str(row['hour']), axis=1)
-    return dff
 
 
+#figure = heatmapp(df.iloc[-2159:])
+#st_pyecharts(figure, height='400px', key='hej')
 
-uge = ugeprofil(df_opti)
-uge2 = ugeprofil(df_norm)
-
-#st.write(uge2)
-
-#st.write(ug)
-#st.write(ug2)
-ugg = uge[['day', 'hour', 'amount_mean', 'x-axis']].merge(uge2[['day', 'hour', 'amount_mean']], how='outer', on=['day', 'hour'], suffixes=('_opti', '_now'))
-ugg['besparelse_kwh'] = ugg['amount_mean_now'] - ugg['amount_mean_opti']
-st.write('Mulig besparelse på ' + str(ugg['besparelse_kwh'].sum()*52) + ' kWh')
-st.write('Årlig forbrug på ' + str(ugg['amount_mean_now'].sum()*52) + ' kWh')
-st.write('Mulig besparelse på ' + str((ugg['besparelse_kwh'].sum()*52)/(ugg['amount_mean_now'].sum()*52)*100) + '%')
 
 
  
